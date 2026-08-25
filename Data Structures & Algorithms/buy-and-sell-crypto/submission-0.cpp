aab9d4017ae9092525b26e9fd2f712d2557804d6
iverson <105322573+anontriton@users.noreply.github.com>
2026-08-25 16:25:38 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0];
+        int sum = 0;
+        
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < min) { min = prices[i]; }
+            if ((prices[i] - min) > sum) { sum = prices[i] - min; } 
+        }
+
+        return sum;
+    }
+};
